2bac5a87b01e72ac0b7e01d3619118235b63a7cc
Romain86 <rmartinet77@gmail.com>
2021-09-07 14:33:46 +0200

    Add entity conference and comment

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,14 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/{name}', name: 'homepage')]
-    public function index(Request $request, string $name): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
-        $greet = sprintf('<h1>Hello %s</h1>', htmlspecialchars($name));
-        return new Response(<<<EOF
+        return new Response(
+            <<<EOF
         <html>
             <body>
-                $greet
                 <img src="/images/under-construction.gif">
             </body>
         </html>
