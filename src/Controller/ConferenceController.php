3334a12f8ea9fe7e183e8c9b2bb6377df68c9360
Romain86 <rmartinet77@gmail.com>
2021-09-08 16:25:02 +0200

    Configure twig template for conferences and comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,24 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Request $request): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(
-            <<<EOF
-        <html>
-            <body>
-                <img src="/images/under-construction.gif">
-            </body>
-        </html>
-        EOF
-        );
+        $conferences = $conferenceRepository->findAll();
+        return $this->render('conference/index.html.twig', ['conferences' => $conferences]);
+    }
+
+    #[Route('/conference/{id}', name:'conference')]
+    public function show($id, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository)
+    {
+        $conference = $conferenceRepository->find($id);
+        $comments = $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']);
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $comments
+        ]);
     }
 }
