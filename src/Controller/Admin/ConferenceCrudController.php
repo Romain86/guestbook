cbb0116d46ebabd0dee8f51a0a73a6e305410094
Romain86 <rmartinet77@gmail.com>
2021-09-08 15:38:55 +0200

    Configure admin

---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use Doctrine\Common\Collections\ArrayCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        yield IdField::new('id')->hideOnForm();
+        yield TextField::new('city');
+        yield TextField::new('year');
+        yield BooleanField::new('isInternational');
+    }
+}
